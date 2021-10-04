abcc876883dccc9285ca25efc055e28b75ae224b
Mollah Tareq <tareqcseuiu@gmail.com>
2021-10-04 23:49:30 +0600

    html css ongoing

---
+++ jscript.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+      id: 0,
+      name: "T-shirt 1",
+      price: 29.99,
+      imgSrc: "./img/t1.png"
+    },
+    {
+      id: 1,
+      name: "T-shirt 2",
+      price: 24.99,
+      imgSrc: "./img/t2.png"
+    },
+    {
+      id: 2,
+      name: "T-shirt 3",
+      price: 19.99,
+      imgSrc: "./img/t3.png"
+    },
+    {
+      id: 3,
+      name: "T-shirt 4",
+      price: 25.99,
+      imgSrc: "./img/t4.png"
+    },
+    {
+      id: 4,
+      name: "T-shirt 5",
+      price: 29.99,
+      imgSrc: "./img/t5.png"
+    },
+    {
+      id: 5,
+      name: "T-shirt 6",
+      price: 39.99,
+      imgSrc: "./img/t6.png"
+    },
+  ];
